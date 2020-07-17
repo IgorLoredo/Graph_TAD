d3ca8ff4d9c1812a1a6eaa13c19aafa5ff87f381
Natã Botelho <nbotelho789@hotmail.com>
2020-07-16 23:44:32 -0300

    novas structs e funções adaptadas

--- Graph.c
+++ Graph.c
@@ -6,14 +6,29 @@
 
 
 Graph *newGraph(){
-    return (Graph)calloc(1,sizeof(Graph));
+    Graph *graph = (Graph*)calloc(1,sizeof(Graph));
+    return graph;
 }
 
-Node *newNode(Char *element,int id){
+Node *newNode(Actor actor, Movie movie){
     Node *new = (Node*)malloc(sizeof(Node));
-    new->Element = element;
-    new->id;
+    new->actor_id = actor.index;
+    new->movie_id = movie.index;
+    new->next = NULL;
+    new->KB = 0;
     return new;
 }
 
- 
+int insertNode(Graph *graph, Node *node, int nactor_id){
+    if(!graph || !node) return ERROR;
+
+    int i = 0;
+    
+    while(i < graph->nElement && graph->list[i]->actor_id != nactor_id)
+        i++;
+
+    node->next = graph->list[i]->next;
+    graph->list[i]->next = node;
+
+    return SUCCESS;
+}
--- Graph.h
+++ Graph.h
@@ -1,28 +1,36 @@
-#ifndef Graph
-#define Graph
+#ifndef _GRAPH_KB
+#define _GRAPH_KB
 
+#define ERROR -1
+#define SUCCESS 1
 
-#define ERRO -1
-#define SUCESS 1
+typedef struct actor{
+    char *name;
+    int index;
+}Actor;
 
-typedef struct node{
-    struct *node;
-    int id;
-    char *Element;
-}Node;
+typedef struct movie{
+    char *name;
+    int index;
+}Movie;
 
+typedef struct node Node;
+struct node{
+    int actor_id;
+    int movie_id;
+    int KB;
+    Node *next;
+};
 
 typedef struct graph{
-   Node **root;
+   Node **list;
    int nElement;
 }Graph;
 
 Graph *newGraph();
 
-Node *newNode(Char *element,int id);
+Node *newNode(Actor, Movie);
 
-int addGraph(Graph *,Node*);
-
-
+int insertGraph(Node);
 
 #endif
